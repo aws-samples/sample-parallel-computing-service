3b7d3f61a6ccc0029ce3492dcf2b17e3a7e1eed8
Timothy Brown <t-brown@users.noreply.github.com>
2026-05-20 09:14:28 -0700

    Refactoring FSx values into variables.
    FSx file systems were hard coded, refactoring them out to
    variables.

--- modules/fsx/variables.tf
+++ modules/fsx/variables.tf
@@ -22,3 +22,55 @@ variable "private_cidr" {
   type = string
 }
 
+
+variable "fsxl" {
+  type = object({
+    deployment_type       = string
+    data_compression_type = string
+    efa_enabled           = bool
+    storage_type          = string
+    throughput_capacity   = number
+    data_read_cache = object({
+      sizing_mode = string
+      size        = number
+    })
+    metadata = object({
+      iops = number
+      mode = string
+    })
+  })
+  default = {
+    deployment_type       = "PERSISTENT_2"
+    data_compression_type = "LZ4"
+    efa_enabled           = true
+    storage_type          = "INTELLIGENT_TIERING"
+    throughput_capacity   = 4000
+    data_read_cache = {
+      sizing_mode = "USER_PROVISIONED"
+      size        = 20000
+    }
+    metadata = {
+      iops = 6000
+      mode = "USER_PROVISIONED"
+    }
+  }
+}
+
+variable "fsxz" {
+  type = object({
+    storage_capacity      = number
+    deployment_type       = string
+    throughput_capacity   = number
+    data_compression_type = string
+    nfs_options           = list(string)
+    volume_nfs_options    = list(string)
+  })
+  default = {
+    storage_capacity      = 1024
+    deployment_type       = "SINGLE_AZ_HA_2"
+    throughput_capacity   = 2560
+    data_compression_type = "ZSTD"
+    nfs_options           = ["sync", "crossmnt", "no_root_squash"]
+    volume_nfs_options    = ["sync", "crossmnt", "no_root_squash", "rw"]
+  }
+}
